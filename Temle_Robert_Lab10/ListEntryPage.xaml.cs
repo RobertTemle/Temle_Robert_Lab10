6a47920efd0fb94f78f470a6ee7bbfc41a24c981
Robert Samuel Temle <Temle.Va.Robert@student.utcluj.ro>
2022-01-13 15:50:04 +0200

    Add project files.

---
+++ Temle_Robert_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Temle_Robert_Lab10.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Temle_Robert_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = await App.Database.GetShopListsAsync();
+        }
+        async void OnShopListAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = new ShopList()
+            });
+        }
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new ListPage
+                {
+                    BindingContext = e.SelectedItem as ShopList
+                });
+            }
+        }
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Temle_Robert_Lab10/Models/ShopList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Temle_Robert_Lab10.Models
+{
+    public class ShopList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date
+        {
+            get; set;
+        }
+    }
+}
